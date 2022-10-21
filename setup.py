4a7b1e72e8f2ceb155485523a4fd965d591a8af1
Anthony Alteirac <anthony@alteirac.com>
2022-10-21 10:27:52 +0200

    remove snowpark deps

--- setup.py
+++ setup.py
@@ -16,5 +16,5 @@ setup(
     author='Brian Hess',
     author_email='brian.hess@snowflake.com',
     license='LICENSE.txt',
-    install_requires=['streamlit', 'snowflake-connector-python', 'snowflake-snowpark-python']
+    install_requires=['streamlit', 'snowflake-connector-python']
 )
