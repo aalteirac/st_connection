2abfc82e71f6dc425104f855700f5c84320c61aa
Anthony Alteirac <anthony@alteirac.com>
2022-10-21 10:23:42 +0200

    remove snowpark deps

--- st_connection/snowflake/snowflake_connection.py
+++ st_connection/snowflake/snowflake_connection.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import st_connection.connection
 from snowflake.connector import SnowflakeConnection, connect
-from snowflake.snowpark import Session
 
 # Implementation that creates Snowflake Connector connections
 class SnowflakeConnectionImpl(st.connection.AbstractConnection):
@@ -23,26 +22,6 @@ class SnowflakeConnectionImpl(st.connection.AbstractConnection):
     def default_form_options(self):
         return {'account': '', 'user': '', 'password': None}
 
-# Implementation that creates Snowflake Snowpark sessions
-class SnowflakeSessionImpl(st.connection.AbstractConnection):
-    def __init__(self):
-        pass
-
-    def is_open(self, conn: Session) -> bool:
-        return not conn._conn._conn.is_closed()
-    
-    def connect(self, params) -> Session:
-        return Session.builder.configs(params).create()
-    
-    def close(self, conn: Session):
-        conn.close()
-
-    def ST_KEY(self):
-        return 'ST_SNOW_SESS'
-    
-    def default_form_options(self):
-        return {'account': '', 'user': '', 'password': None}
 
 class snowflake:
     connection = st.connection.connection(SnowflakeConnectionImpl())
-    session = st.connection.connection(SnowflakeSessionImpl())
