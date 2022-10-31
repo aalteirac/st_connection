cb8fbfca63dae5a447b2ba19281319b57a06636a
aalteirac <anthony@alteirac.com>
2022-10-31 11:00:10 +0100

    bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 setup(
     name="st_connection",
     packages=find_packages(),
-    version='0.0.1',
+    version='0.0.2',
     description='Helper tools for connecting to data sources from Streamlit',
     long_description=long_description,
     long_description_content_type="text/markdown",
