a3285bd514f4374a79de5e6471881555c87186ee
aalteirac <anthony@alteirac.com>
2022-10-31 12:56:07 +0100

    test selectbox

--- st_connection/connection.py
+++ st_connection/connection.py
@@ -97,7 +97,10 @@ class _connection:
         ST_FORM_KEY = f'{ST_KEY}_FORM'
         with st.form(form_title):
             for k,v in form_options.items():
-                st.text_input(k.capitalize(), value="" if v is None else v, key=f"{ST_FORM_KEY}{k}", type="password" if v is None else "default")
+                if len(v.split("##"))>0:
+                    st.selectbox(k.capitalize(), options==v.split("##") , key=f"{ST_FORM_KEY}{k}")
+                else:
+                    st.text_input(k.capitalize(), value="" if v is None else v, key=f"{ST_FORM_KEY}{k}", type="password" if v is None else "default")
             st.form_submit_button("Connect", on_click=_callback_and_clear, args=(self._connection_impl.connect_and_cache, ST_FORM_KEY, options))
         st.stop()
 
